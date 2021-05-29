39ac13dcc1f59090b9163447300e482a4414cff4
jaikko <65661383+jaikko@users.noreply.github.com>
2021-05-29 16:52:48 +0200

    Update optimized.py

--- optimized.py
+++ optimized.py
@@ -1,50 +1,80 @@
-import csv, time
+import csv
+import time
 import os
-from operator import itemgetter
+import math
 
+# A Dynamic Programming based Python
 
 # Choix du fichier
 file = input("Entrez le nom du fichier csv: ")
 
-while not os.path.exists("data/"+ file + ".csv"):
-    
+while not os.path.exists("data/" + file + ".csv"):
     print("Fichier introuvable ou mauvais type de fichier")
     file = input("Entrez le nom du fichier csv: ")
-    
+
+# Variable
+names = []
+prices = []
+profits = []
 
 # Démarrer chrono
 start_time = time.time()
 
 # Lecture du fichier excel
-liste = []
-with open("data/"+ file + ".csv", newline='') as csvfile:
+with open("data/" + file + ".csv", newline='') as csvfile:
     spamreader = csv.reader(csvfile)
     next(csvfile)
     for row in spamreader:
         info = ''.join(row).split(';')
-        name, price, profit = str(info[0]), float(info[1]), float(info[2])
+        name, price, price_wr, profit = str(info[0]), math.floor(float(info[1])), float(info[1]), float(info[2])
         if price > 0 and profit > 0:
-            sum = price*profit/100
-            ratio = price/sum
-            dict = {'name': name, 'price': price, 'profit': sum, 'ratio': ratio}
-            liste.append(dict)
+            names.append(name)
+            prices.append(price)
+            profits.append(math.floor(price_wr * profit/100))
 
-# Trier le élémenents par ratio
-ordered_list = sorted(liste, key=itemgetter('ratio'), reverse=False)
 
-# Calcul du meilleur investissment
+# Fonction  pour calculer le meilleur gain
+def knapSack(W, prices, profits, n):
+    # Création du tableau K[][]
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+
+    for i in range(n + 1):
+        for w in range(W + 1):
+
+            if i == 0 or w == 0:
+                K[i][w] = 0
+
+            elif prices[i-1] <= w:
+                K[i][w] = max(profits[i-1] + K[i-1][w-prices[i-1]], K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+    return K, K[n][W]
+
+
+W = 500
+n = len(names)
+
+# Obtenir le meilleur gain
+K, gain = knapSack(W, prices, profits, n)
+print("Gain: " + str(gain) + " euros")
+
+w = W
 total = 0
-list_buy = []
-gain = 0
-for action in ordered_list:
-        name, price, profit = str((action["name"])), float(action["price"]), float(action["profit"])
-        if total + price <= 500:
-            total += price
-            gain += profit
-            list_buy.append(name)
+buy = []
 
+# Récuper la somme investie et le nom des actions
+for i in range(n, 0, -1):
+    if gain <= 0:
+        break
 
-print("Vous devez acheter des actions chez " + ', '.join(list_buy) + '.')
-print("Avec un investissement de " + str(round(total, 2)) + " euros" + ',vous gagnerez ' + str(round(gain, 2)) + " euros")
+    if gain == K[i - 1][w]:
+        continue
+    else:
+        total += prices[i - 1]
+        buy.append(names[i - 1])
+        gain = gain - profits[i - 1]
+        w = w - prices[i - 1]
+
+print("Somme investie : " + str(total) + " euros")
+print("Action(s) : " + ", ".join(buy))
 print("--- %s seconds ---" % (time.time() - start_time))
-
